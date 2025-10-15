7c2d3cc14bf5ed3d07349561112ee3c6667edcca
Nathan Ulmen <147746867+NathanUlmen@users.noreply.github.com>
2025-10-15 16:40:26 -0600

    Renders both a plane and a cube ontop of that plane.

--- core/src/main/java/ore/forge/Screens/Gameplay3D.java
+++ core/src/main/java/ore/forge/Screens/Gameplay3D.java
@@ -14,75 +14,81 @@ import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 import com.badlogic.gdx.graphics.glutils.VertexArray;
 import com.badlogic.gdx.math.Matrix3;
 import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Vector3;
 import ore.forge.Input.CameraController3D;
+
+import java.util.ArrayList;
 
 public class Gameplay3D implements Screen {
     private final PerspectiveCamera camera;
-    private final ModelBatch  modelBatch;
+    private final ModelBatch modelBatch;
     private final Environment environment;
-    private CameraInputController cameraController;
     private final CameraController3D cameraController3D;
+    private final ArrayList<ModelInstance> modelInstances;
 
-    private final Model model;
-    private final ModelInstance instance;
+    private Model model;
+    private ModelInstance instance;
 
     public Gameplay3D() {
         //Config camera
-        camera  = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        camera = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         camera.near = 1f; //Min distance can see/draw
         camera.far = 1000f; //Max distance can see/draw
         camera.position.set(10, 10, 10);
         camera.lookAt(0, 0, 0);
 
         //Config cameraController;
-//        cameraController = new CameraInputController(camera);
         cameraController3D = new CameraController3D(camera);
 
         //Config ModelBatch
         modelBatch = new ModelBatch();
 
+        //Config Model Instance list
+        modelInstances = new  ArrayList<>();
 
         //Config Environment
         environment = new Environment();
         environment.set(new ColorAttribute(ColorAttribute.AmbientLight, .8f, .8f, .8f, 1f));
         environment.add(new DirectionalLight().set(1f, 1f, 1f, -1f, -0.8f, -0.2f));
 
-
-        // Create a test object to draw
-        ModelBuilder builder = new ModelBuilder();
-        model = builder.createBox(5,5,5,
-            new Material(ColorAttribute.createDiffuse(Color.GREEN)),
-            VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
-        instance = new ModelInstance(model);
-
-
-
+        //create cube and apply transform
         Matrix4 transform = new Matrix4();
-        transform = transform.translate(0,0,0);
+        model = createBox(5, 5, 5, Color.PINK);
+        var instance = new ModelInstance(model);
+        transform.translate(0, 2.5f,0);
         instance.transform.set(transform);
+        modelInstances.add(instance);
+
+        //create plane and apply transform
+        instance = new ModelInstance(createBox(50, 0.01f , 50));
+        var t2 = new Matrix4();
+        t2.translate(0, 0, 0);
+        instance.transform.set(t2);
+        modelInstances.add(instance);
+
     }
 
     @Override
     public void show() {
-        Gdx.input.setInputProcessor(cameraController);
 
     }
 
     @Override
     public void render(float delta) {
-//        cameraController.update();
         cameraController3D.update();
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
         camera.update();
-        modelBatch.begin(camera); modelBatch.render(instance, environment);
+        modelBatch.begin(camera);
+        for (var instance : modelInstances) {
+            modelBatch.render(instance, environment);
+        }
         modelBatch.end();
         System.out.println(Gdx.graphics.getFramesPerSecond());
     }
 
     @Override
     public void resize(int width, int height) {
-
 
     }
 
@@ -105,5 +111,17 @@ public class Gameplay3D implements Screen {
     public void dispose() {
 
     }
+
+    private Model createBox(float width, float height, float depth, Color color) {
+        ModelBuilder builder = new ModelBuilder();
+        return builder.createBox(width, height, depth,
+            new Material(ColorAttribute.createDiffuse(color)),
+            VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
+    }
+
+    private Model createBox(float width, float height, float depth) {
+        return createBox(width, height, depth, Color.GREEN);
+    }
+
 
 }
